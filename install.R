042903dd43ea4879fb5766b3e7f2ad9016be64a8
Stephen J. Eglen <sje30@cam.ac.uk>
2018-06-16 19:57:49 +0100

    Add sje... packages from damtp.cam.ac.uk

--- install.R
+++ install.R
@@ -1,5 +1,7 @@
 install.packages(c("splancs", "spatstat", "knitr", "devtools"))
 
-devtools::install_github('sje30/sjedrp')
+## devtools::install_github('sje30/sjedrp')
+
+install.packages(c("sjedmin", "sjedrp", "sjevor","sjedist"), type="source", contriburl="http://damtp.cam.ac.uk/user/eglen/r/")
 
 
